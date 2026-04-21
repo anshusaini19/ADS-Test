586abe6a7e2df84fce40d0fbdfb0654d0bc6d191
anshusaini19 <157450856+anshusaini19@users.noreply.github.com>
2026-04-21 14:35:21 +0530

    Create UniquePathsII.java

---
+++ UniquePathsII.java
@@ -0,0 +1,71 @@
+public class UniquePathsII {
+
+    public static int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if (grid[0][0] == 1) return 0;
+
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++) {
+            if (grid[i][0] == 0) {
+                dp[i][0] = dp[i - 1][0];
+            }
+        }
+
+        for (int j = 1; j < n; j++) {
+            if (grid[0][j] == 0) {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
+    public static void printGrid(int[][] grid) {
+        for (int[] row : grid) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void runTest(int[][] grid) {
+        System.out.println("Input Grid:");
+        printGrid(grid);
+        int result = uniquePathsWithObstacles(grid);
+        System.out.println("Unique Paths: " + result);
+        System.out.println("----------------------");
+    }
+
+    public static void main(String[] args) {
+
+        int[][] grid1 = {
+            {0, 0, 0},
+            {0, 1, 0},
+            {0, 0, 0}
+        };
+
+        int[][] grid2 = {
+            {0, 1},
+            {0, 0}
+        };
+
+        int[][] grid3 = {
+            {1}
+        };
+
+        runTest(grid1);
+        runTest(grid2);
+        runTest(grid3);
+    }
+}
