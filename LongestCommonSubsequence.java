9661ac55b5ca7ed2d94861807cf4c17aadac7d98
anshusaini19 <157450856+anshusaini19@users.noreply.github.com>
2026-04-21 14:38:19 +0530

    Create LongestCommonSubsequence.java

---
+++ LongestCommonSubsequence.java
@@ -0,0 +1,35 @@
+public class LongestCommonSubsequence {
+
+    public static int lcs(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void runTest(String s1, String s2) {
+        System.out.println("Input: \"" + s1 + "\", \"" + s2 + "\"");
+        int result = lcs(s1, s2);
+        System.out.println("LCS Length: " + result);
+        System.out.println("----------------------");
+    }
+
+    public static void main(String[] args) {
+
+        runTest("abcde", "ace"); // 3
+        runTest("abc", "abc");   // 3
+        runTest("abc", "def");   // 0
+    }
+}
