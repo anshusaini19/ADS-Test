701932d2c5b1a177a95d1365d53031ebce6c9792
anshusaini19 <157450856+anshusaini19@users.noreply.github.com>
2026-04-21 14:39:46 +0530

    Create EditDistance.java

---
+++ EditDistance.java
@@ -0,0 +1,48 @@
+public class EditDistance {
+
+    public static int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        // base cases
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        // fill dp table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int insert = dp[i][j - 1];
+                    int delete = dp[i - 1][j];
+                    int replace = dp[i - 1][j - 1];
+
+                    dp[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void runTest(String s1, String s2) {
+        System.out.println("Input: \"" + s1 + "\", \"" + s2 + "\"");
+        int result = minDistance(s1, s2);
+        System.out.println("Edit Distance: " + result);
+        System.out.println("----------------------");
+    }
+
+    public static void main(String[] args) {
+
+        runTest("horse", "ros");         // 3
+        runTest("intention", "execution"); // 5
+        runTest("abc", "yabd");          // 2
+    }
+}
